0132ad0c179aa373bdccedca23a7d00efc080ebe
Ziqin Qu <Ziqin.Qu@gmail.com>
2024-01-31 23:54:27 +0800

    Update MessageParser.cs

--- csharp/src/Google.Protobuf/MessageParser.cs
+++ csharp/src/Google.Protobuf/MessageParser.cs
@@ -246,7 +246,7 @@ namespace Google.Protobuf
         /// Creates a template instance ready for population.
         /// </summary>
         /// <returns>An empty message.</returns>
-        internal new T CreateTemplate()
+        public new T CreateTemplate()
         {
             return factory();
         }
